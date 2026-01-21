ef09614b36d8b593b138851ef98530acee3c764d
Backend12 <backend12@Erxess-Mac-mini.local>
2026-01-21 20:23:45 +0800

    .

--- src/backend/timeManage/graphql/query.ts
+++ src/backend/timeManage/graphql/query.ts
@@ -5,11 +5,10 @@ export const timeQueryResolvers = {
     const times = await TimeManage.find();
     return times;
   },
-  filterTimes: async (_: any, args: { username: string; services: string }) => {
+  filterTimes: async (_: any, args: { username: string }) => {
     const times = await TimeManage.find(
       {
         username: args.username,
-        services: args.services,
       },
       { startTime: 1, endTime: 1, _id: 0, services: 1, username: 1 },
     );
